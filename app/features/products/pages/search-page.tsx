2ed58e4a78355f9f4d64efc1e25a1ba503de5420
Jaewon <sorhd134@gmail.com>
2025-02-09 13:17:41 +0900

    feat: Search Page

--- app/features/products/pages/search-page.tsx
+++ app/features/products/pages/search-page.tsx
@@ -1,4 +1,11 @@
+import { z } from "zod";
 import type { Route } from "./+types/search-page";
+import Hero from "~/common/components/hero";
+import { ProductCard } from "../components/product-card";
+import ProductPagination from "~/common/components/product-pagination";
+import { Form } from "react-router";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,10 +14,53 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
+
+export function loader({ request }: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const { success, data: parsedData } = paramsSchema.safeParse(
+    Object.fromEntries(url.searchParams)
+  );
+
+  if (!success) {
+    throw new Error("Invalid params");
+  }
+
+  console.log(parsedData);
+}
+
 export default function SearchPage() {
   return (
-    <div>
-      <h1>Search Page</h1>
+    <div className="space-y-10">
+      <Hero
+        title="Search"
+        subtitle="Search for products by title or description"
+      />
+      <Form className="flex justify-center max-w-screen-sm mx-auto gap-2">
+        <Input
+          name="query"
+          placeholder="Search for products"
+          className="text-lg"
+        />
+        <Button type="submit">Search</Button>
+      </Form>
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id="productId"
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={12}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
 }
