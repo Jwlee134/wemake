650f46f7d0c2e3ed204e23190ef9f74a2203ece2
Jaewon <sorhd134@gmail.com>
2025-02-15 11:55:31 +0900

    feat: Dashboard Analytics

--- app/features/users/pages/dashboard-page.tsx
+++ app/features/users/pages/dashboard-page.tsx
@@ -1,9 +1,81 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
 import type { Route } from "./+types/dashboard-page";
+import { Line } from "recharts";
+import {
+  ChartTooltipContent,
+  type ChartConfig,
+} from "~/common/components/ui/chart";
+import { ChartTooltip } from "~/common/components/ui/chart";
+import { XAxis } from "recharts";
+import { CartesianGrid } from "recharts";
+import { ChartContainer } from "~/common/components/ui/chart";
+import { LineChart } from "recharts";
 
 export function meta({ matches }: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
 }
 
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+];
+const chartConfig = {
+  views: {
+    label: "👀",
+    color: "hsl(var(--primary))",
+  },
+} satisfies ChartConfig;
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="space-y-10">
+      <h1 className="text-2xl font-semibold mb-6">Dashboard</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Profile Views</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey="views"
+                type="natural"
+                stroke="var(--color-views)"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
