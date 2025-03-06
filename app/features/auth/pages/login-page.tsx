9b9fb053f47cd5c2d17ab288bb138f3d609d24e8
이재원 <sorhd134@gmail.com>
2025-03-06 18:29:52 +0900

    feat: Log In

--- app/features/auth/pages/login-page.tsx
+++ app/features/auth/pages/login-page.tsx
@@ -1,9 +1,11 @@
-import { data, Form, Link, useNavigation } from "react-router";
+import { data, Form, Link, redirect, useNavigation } from "react-router";
 import type { Route } from "./+types/login-page";
 import InputWithLabel from "~/common/components/input-with-label";
 import { Button } from "~/common/components/ui/button";
 import AuthButtons from "../components/auth-buttons";
 import { LoaderCircleIcon } from "lucide-react";
+import { z } from "zod";
+import { getServerClient } from "~/supa-client";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,13 +14,47 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function action({ request }: Route.ActionArgs) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const formData = await request.formData();
-  const email = formData.get("email");
-  const password = formData.get("password");
+const formSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email should be a string",
+    })
+    .email({ message: "Invalid email" }),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password should be a string",
+    })
+    .min(8, { message: "Password should be at least 8 characters long" }),
+});
 
-  return data({ message: "gagaga" });
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const {
+    success,
+    data: loginData,
+    error,
+  } = formSchema.safeParse(Object.fromEntries(formData));
+
+  if (!success) {
+    return { formErrors: error.flatten().fieldErrors, loginError: null };
+  }
+
+  const { email, password } = loginData;
+
+  const { client, headers } = getServerClient(request);
+  const { error: loginError } = await client.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (loginError) {
+    return { formErrors: null, loginError: loginError.message };
+  }
+
+  return redirect("/", { headers });
 }
 
 export default function LoginPage({ actionData }: Route.ComponentProps) {
@@ -42,6 +78,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             placeholder="e.g. john_doe@example.com"
             required
           />
+          {actionData?.formErrors?.email && (
+            <p className="text-red-500 text-xs font-medium">
+              {actionData.formErrors.email.join(", ")}
+            </p>
+          )}
           <InputWithLabel
             id="password"
             label="Password"
@@ -51,6 +92,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             placeholder="Enter your password"
             required
           />
+          {actionData?.formErrors?.password && (
+            <p className="text-red-500 text-xs font-medium">
+              {actionData.formErrors.password.join(", ")}
+            </p>
+          )}
           <Button type="submit" className="w-full" disabled={isSubmitting}>
             {isSubmitting ? (
               <LoaderCircleIcon className="animate-spin" />
@@ -58,6 +104,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
               "Login"
             )}
           </Button>
+          {actionData?.loginError && (
+            <p className="text-red-500 text-xs font-medium">
+              {actionData.loginError}
+            </p>
+          )}
         </Form>
         <AuthButtons />
       </div>
